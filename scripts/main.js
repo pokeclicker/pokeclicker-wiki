7a558fd33b1299a01f8e20ecb0528d80465aaadc
Danial <admin@redsparr0w.com>
2022-12-22 22:03:43 +1300

    Basic markdown and ajax

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,27 @@
-var pageType = ko.observable();
-var pageName = ko.observable();
+
+// Setup our markdown editor
+const md = new markdownit();
+
+md.renderer.rules.table_open = function(tokens, idx) {
+    return '<table class="table table-hover table-striped table-bordered">';
+};
+
+// Setup our pages
+const pageType = ko.observable();
+const pageName = ko.observable();
+const pageOutput = ko.observable('');
+
+// Download remote data when pagename changes
+pageName.subscribe((v)=>{
+    pageOutput('');
+    fetch(`./data/${pageType()}/${pageName()}.md`)
+        .then((response) => response.status == 200 ? response.text() : '')
+        .then((data) => {
+            if (!data) return;
+            pageOutput(md.render(data));
+        });
+});
+
 $('document').off('ready');
 $(document).ready(() => {
     pageType($('#page-type').val());
@@ -10,8 +32,7 @@ window.onbeforeunload = () => {
     Settings.saveDefault();
 }
 
-
-// Map hints to requirements
+// Map our requirment hints to the requirement
 Requirement.prototype.toJSON = function() {
     const req = this.__proto__.constructor.name === 'LazyRequirementWrapper'
         ? this.unwrap()
@@ -24,10 +45,10 @@ Requirement.prototype.toJSON = function() {
     };
 };
 
+// Custom binds as these aren't loaded
 App.game = {
     breeding: new Breeding(),
 }
-
 
 // TODO: Fix these up somehow..
 // Overrides, these methods don't work if game not started..
