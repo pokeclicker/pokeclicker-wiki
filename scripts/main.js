ad67b73ad64a6c9f37cd9b7bd0717a053e374e03
Danial <admin@redsparr0w.com>
2022-12-22 23:06:28 +1300

    Moved pages to own folder, Fixed up loading md and pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,39 @@
 function onPageChange() {
-    var pageElement = $('#wiki-page-content');
+    const pageElement = $('#wiki-page-content');
     pageElement.html(''); //TODO: spinner?
+    let page = '';
+    window.location.hash = `#!${pageType()}/${pageName()}`;
     if (!pageType()&&!pageName()) {
-        $.get('home.html', function(data) {
-            pageElement.html(data);
-        });
+        page = 'pages/home.html';
     } else if (!pageName()) {
-        $.get(pageType() + '/overview.html', function(data) {
-            pageElement.html(data);
-        });
+        page = `pages/${pageType()}/overview.html`;
     } else {
-        $.get(pageType() + '/main.html', function(data) {
-            pageElement.html(data);
-            console.log(data);
-        });
+        page = `pages/${pageType()}/main.html`;
     }
+    $.get(page, function(data) {
+        pageElement.html(data);
+        applyBindings(true);
+    });
+
+    const pageElementCustom = $('#wiki-page-custom-content');
+    pageElementCustom.html('');
+    $.get(`data/${pageType()}/${pageName()}.md`, function(data) {
+        pageElementCustom.html(md.render(data));
+    });
 }
 
 // Setup our markdown editor
 const md = new markdownit();
-
 md.renderer.rules.table_open = function(tokens, idx) {
     return '<table class="table table-hover table-striped table-bordered">';
 };
 
-// Setup our pages
+// Setup our pages observables
 const pageType = ko.observable();
 const pageName = ko.observable();
-const pageOutput = ko.observable('');
+const applyBindings = ko.observable(false);
 
-
-// Download remote data when pagename changes
-pageName.subscribe((v)=>{
-    pageOutput('');
-    window.location.hash = `#!${pageType()}/${pageName()}`;
-    fetch(`./data/${pageType()}/${pageName()}.md`)
-        .then((response) => response.status == 200 ? response.text() : '')
-        .then((data) => {
-            if (!data) return;
-            pageOutput(md.render(data));
-        });
-});
-
+// Load the page the user is trying to visit
 (() => {
     const [ type, name ] = window.location.hash.substr(2).split('/');
     pageType(decodeURI(type || ''));
@@ -50,15 +42,22 @@ pageName.subscribe((v)=>{
 
 $('document').off('ready');
 $(document).ready(() => {
-    pageType($('#page-type').val());
-    pageName($('#page-name').val());
     pageType.subscribe(onPageChange);
     pageName.subscribe(onPageChange);
+    applyBindings.subscribe((v) => {
+        // This doesn't work as we can only bind to an element once..
+        if (v) {
+            applyBindings(false);
+            ko.applyBindings({}, document.getElementById('wiki-page-content'))
+        }
+    })
 
     onPageChange();
     
-    ko.applyBindings({});
+    ko.applyBindings({}, document.getElementById('nav-bar'));
 });
+
+// Save any settings the user has set
 window.onbeforeunload = () => {
     Settings.saveDefault();
 }
@@ -85,3 +84,82 @@ App.game = {
 // Overrides, these methods don't work if game not started..
 PokemonHelper.getPokemonWandering = () => [];
 PokemonHelper.getPokemonDiscord = () => [];
+
+
+/*
+    AUTO FILL FOR SEARCH BAR
+*/
+const searchOptions = [
+    {
+        display:'Items',
+        type: 'items',
+        page: '',
+    },
+    {
+        display:'Pokémon',
+        type: 'pokemon',
+        page: '',
+    },
+    ...Object.values(ItemList).map(i => ({
+        display: i.displayName,
+        type: 'items', 
+        page: i.displayName,
+    })),
+    ...Object.values(pokemonList).map(p => ({
+        display: p.name,
+        type: 'pokemon',
+        page: p.name,
+    })),
+    ...Object.values(dungeonList).map(d => ({
+        display: d.name,
+        type: 'dungeons',
+        page: d.name,
+    })),
+];
+var substringMatcher = function(searchData) {
+  return function findMatches(query, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(query, 'ig');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    searchData.forEach((d, i) => {
+      if (substrRegex.test(d.display)) {
+        matches.push(d);
+      }
+    });
+    cb(matches);
+  };
+};
+
+$('#search').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1,
+  classNames: {
+    menu: 'dropdown-menu',
+    suggestion: 'dropdown-item',
+    cursor: 'active',
+  }
+},
+{
+  name: 'Search',
+  source: substringMatcher(searchOptions),
+  display: 'display',
+  templates: {
+    notFound: '<a class="dropdown-item disabled">No results found...</a>',
+  }
+});
+$('#search').bind('typeahead:select', function(ev, suggestion) {
+    pageType(suggestion.type);
+    pageName(suggestion.page);
+});
+$('#search').bind('typeahead:autocomplete', function(ev, suggestion) {
+    pageType(suggestion.type);
+    pageName(suggestion.page);
+});
