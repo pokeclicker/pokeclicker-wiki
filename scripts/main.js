47d8ba60c3fc19343c0af072e3266d33c62fd9c7
jaaslet <jonas_bms@hotmail.com>
2022-12-23 12:35:59 +0100

    Attept at cleaning up breadcrumbs

--- scripts/main.js
+++ scripts/main.js
@@ -76,13 +76,14 @@ $(document).ready(() => {
   window.location.hash = '#!loading';
   gotoPage(decodeURIComponent(type || ''), decodeURIComponent(name || ''));
 
+  ko.applyBindings({}, document.getElementById('page-nav'));
   ko.applyBindings({}, document.getElementById('nav-bar'));
   applyBindings.subscribe((v) => {
     // This doesn't work as we can only bind to an element once..
     if (v) {
       applyBindings(false);
       ko.cleanNode(document.getElementById('wiki-page-content'));
-      ko.applyBindings({}, document.getElementById('wiki-page-content'));
+      ko.applyBindings({}, document.getElementById('wiki-page-content'));''
     }
   });
 });
