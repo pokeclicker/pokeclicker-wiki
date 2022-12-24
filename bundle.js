a4ea8bc8de7e3133fb6dc85d8f98fb45fbc2a877
Danial <admin@redsparr0w.com>
2022-12-24 15:25:17 +1300

    Allow linking to a singular/main page

--- bundle.js
+++ bundle.js
@@ -11001,13 +11001,13 @@ var Plugin = require('markdown-it-regexp');
 
 var plugin = Plugin(
   // regexp to match
-  /\[\[([^|\]]+)\/([^|\]]+)\]\]/,
+  /\[\[([^|\]]+)(\/([^|\]]+))?\]\]/,
 
   // this function will be called when something matches
   (match, utils) => {
-    var url = `gotoPage('${utils.escape(match[1])}', '${utils.escape(match[2])}')`;
+    var url = `gotoPage('${utils.escape(match[1])}', '${utils.escape(match[3] || '')}')`;
 
-    return `<a href="#!${utils.escape(match[1])}/${utils.escape(match[2])}" onclick="${url}; return false;">${utils.escape(match[2])}</a>`;
+    return `<a href="#!${utils.escape(match[1])}/${utils.escape(match[3] || '')}" onclick="${url}; return false;">${utils.escape(match[3] || match[1])}</a>`;
   }
 );
 
