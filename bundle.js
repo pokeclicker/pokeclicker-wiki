1365648395ff592ceb02cf6d571e5640db30b9b1
Danial <admin@redsparr0w.com>
2023-01-03 16:25:01 +1300

    Optimize Vitamin calculation code

--- bundle.js
+++ bundle.js
@@ -11344,46 +11344,63 @@ getEff = (vitaminsUsed, baseAttack, eggCycles) => {
 }
 
 calculateVitamins = (att, steps, region) => {
-  vitaminsUsed = {};
-  data = [];
-  totalVitamins = (region + 1) * 5;
-  for(protein = 0; protein <= totalVitamins; protein++) {
-    vitaminsUsed[GameConstants.VitaminType.Protein] = protein
-    if (region < GameConstants.Region.hoenn) {
-        vitaminsUsed[GameConstants.VitaminType.Calcium] = 0
-        vitaminsUsed[GameConstants.VitaminType.Carbos] = 0
-        data.push({
-            protein,
-            calcium: 0,
-            carbos: 0,
-            eff: getEff(vitaminsUsed, att, steps),
-        });
-    } else {
-        for(calcium = 0; calcium <= (totalVitamins - protein); calcium++) {
-            vitaminsUsed[GameConstants.VitaminType.Calcium] = calcium
-            if (region < GameConstants.Region.unova) {
-                vitaminsUsed[GameConstants.VitaminType.Carbos] = 0
-                data.push({
-                    protein,
-                    calcium,
-                    carbos: 0,
-                    eff: getEff(vitaminsUsed, att, steps),
-                });
-            } else {
-                for(carbos = 0; carbos <= (totalVitamins - (protein + calcium)); carbos++) {
-                    vitaminsUsed[GameConstants.VitaminType.Carbos] = carbos
+    vitaminsUsed = {};
+    data = [];
+    totalVitamins = (region + 1) * 5;
+    protein = totalVitamins + 1;
+    while (protein-- > 0) {
+        vitaminsUsed[GameConstants.VitaminType.Protein] = protein
+        if (region < GameConstants.Region.hoenn) {
+            // We can't access these yet
+            vitaminsUsed[GameConstants.VitaminType.Calcium] = 0
+            vitaminsUsed[GameConstants.VitaminType.Carbos] = 0
+            const eff = getEff(vitaminsUsed, att, steps);
+            // If the previous result is better than this, no point to continue
+            if (eff < data[data.length - 1]?.eff) break;
+            // Push our data
+            data.push({
+                protein,
+                calcium: 0,
+                carbos: 0,
+                eff: getEff(vitaminsUsed, att, steps),
+            });
+        } else {
+            calcium = (totalVitamins - protein) + 1;
+            while (calcium-- > 0) {
+                vitaminsUsed[GameConstants.VitaminType.Calcium] = calcium
+                if (region < GameConstants.Region.unova) {
+                    // We can't access these yet
+                    vitaminsUsed[GameConstants.VitaminType.Carbos] = 0
+                    const eff = getEff(vitaminsUsed, att, steps);
+                    // If the previous result is better than this, no point to continue
+                    if (eff < data[data.length - 1]?.eff) break;
+                    // Push our data
                     data.push({
                         protein,
                         calcium,
-                        carbos,
-                        eff: getEff(vitaminsUsed, att, steps),
+                        carbos: 0,
+                        eff,
                     });
+                } else {
+                    carbos = (totalVitamins - (protein + calcium)) + 1;
+                    while (carbos-- > 0) {
+                        vitaminsUsed[GameConstants.VitaminType.Carbos] = carbos
+                        const eff = getEff(vitaminsUsed, att, steps);
+                        // If the previous result is better than this, no point to continue
+                        if (eff < data[data.length - 1]?.eff) break;
+                        // Push our data
+                        data.push({
+                        protein,
+                        calcium,
+                        carbos,
+                        eff,
+                        });
+                    }
                 }
             }
         }
     }
-  }
-  return data;
+    return data;
 }
 
 const getBestVitamins = (att, step, region) => {
