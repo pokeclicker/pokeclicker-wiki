013b8d3a8a05b427c21be356cfb5b0e5c9eb42ce
Danial <admin@redsparr0w.com>
2023-02-09 11:36:08 +1300

    Allow é in filenames, as we use it semi often

--- bundle.js
+++ bundle.js
@@ -12757,7 +12757,7 @@ onhashchange = (event) => {
     pageElement.html(errorPage);
   });
 
-  const cleanFileName = (s) => s.replace(/[^\s\w\(\)'"!-]/gi, "-");
+  const cleanFileName = (s) => s.replace(/[^\s\w\(\)'"!-é]/gi, "-");
 
   const pageElementCustom = $('#wiki-page-custom-content');
   pageElementCustom.html('');
