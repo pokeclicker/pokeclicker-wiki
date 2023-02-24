fa490629b8a0103e0343f8249374d05ac81e3ad6
PixLSteam <PixLSteam@users.noreply.github.com>
2023-02-24 16:50:18 +0100

    Attempt to fix the broken images

--- bundle.js
+++ bundle.js
@@ -12964,6 +12964,7 @@ module.exports = {
 }
 
 },{}],116:[function(require,module,exports){
+//TODO: remove duplicate (meant as an example)
 const redirections = [
     ({type, name}) => {
         if (type === 'Pokemon') {
