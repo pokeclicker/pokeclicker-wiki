b64263cfaceb12f5964e6243e156ced725aace51
CypherX <cypherx00@gmail.com>
2023-02-26 17:11:04 -0500

    Daily deals

--- bundle.js
+++ bundle.js
@@ -12422,6 +12422,10 @@ QuestLineHelper.loadQuestLines();
 BattleFrontierRunner.stage(100);
 BattleFrontierBattle.generateNewEnemy();
 
+const now = new Date();
+DailyDeal.generateDeals(5, now);
+BerryDeal.generateDeals(now);
+
 // Map our requirment hints to the requirement
 Requirement.prototype.toJSON = function() {
   const req = this.__proto__.constructor.name === 'LazyRequirementWrapper'
