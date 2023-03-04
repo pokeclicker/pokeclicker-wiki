2d42908d9053a7b44d6a52977f3c7afe35a8eae0
PixLSteam <PixLSteam@users.noreply.github.com>
2023-03-04 04:03:00 +0100

    Allow matching e and é (#33)
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>

--- bundle.js
+++ bundle.js
@@ -13204,7 +13204,7 @@ searchOptions.forEach(a => {
 */
 
 function escapeRegExp(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&').replace(/[eé]/g, '[eé]');
 }
 // This is the function which figures out the results to show
 var substringMatcher = (searchData) => {
