de1462c865b5e5b4924af8de7a36cd0dea151b8f
CypherX <cypherx00@gmail.com>
2023-03-19 23:21:23 -0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -12416,6 +12416,7 @@ App.game = new Game(
   new DreamOrbController()
 );
 App.game.farming.initialize();
+App.game.farming.plotList.forEach((p) => p.isUnlocked = true);
 App.game.breeding.initialize();
 SafariPokemonList.generateSafariLists();
 QuestLineHelper.loadQuestLines();
@@ -12470,10 +12471,11 @@ window.Wiki = {
   ...require('./discord'),
   pokemon: require('./pages/pokemon'),
   dreamOrbs: require('./pages/dreamOrbs'),
+  farmSimulator: require('./pages/farmSimulator'),
   ...require('./navigation'),
 }
 
-},{"../pokeclicker/package.json":101,"./datatables":102,"./discord":103,"./game":104,"./markdown-renderer":111,"./navigation":112,"./notifications":113,"./pages/dreamOrbs":114,"./pages/pokemon":115,"./typeahead":117}],106:[function(require,module,exports){
+},{"../pokeclicker/package.json":101,"./datatables":102,"./discord":103,"./game":104,"./markdown-renderer":111,"./navigation":112,"./notifications":113,"./pages/dreamOrbs":114,"./pages/farmSimulator":115,"./pages/pokemon":116,"./typeahead":118}],106:[function(require,module,exports){
 const { md } = require('./markdown-renderer');
 
 const saveChanges = (editor, filename, btn) => {
@@ -12863,7 +12865,7 @@ module.exports = {
     gotoPage,
 };
 
-},{"./datatables":102,"./markdown-editor":106,"./markdown-renderer":111,"./redirections":116}],113:[function(require,module,exports){
+},{"./datatables":102,"./markdown-editor":106,"./markdown-renderer":111,"./redirections":117}],113:[function(require,module,exports){
 const alert = (message, type = 'primary', timeout = 5e3) => {
   const wrapper = document.createElement('div');
   wrapper.classList.add('alert', `alert-${type}`, 'alert-dismissible', 'fade', 'show');
@@ -12904,6 +12906,179 @@ module.exports = {
 };
 
 },{}],115:[function(require,module,exports){
+const selectedPlot = ko.observable(undefined);
+const possibleMutations = ko.observable([]);
+
+const selectPlot = (plotIndex) => {
+    selectedPlot(App.game.farming.plotList[plotIndex]);
+    $('#plotList > .plot > .plot-content').removeClass('selected');
+    $(`#plotList > .plot:eq(${plotIndex}) > .plot-content`).addClass('selected');
+};
+
+const getImage = (plot) => {
+    if (plot.berry === BerryType.None) {
+        return '';
+    }
+    if (plot.stage() === PlotStage.Seed) {
+        return './images/farm/AllTreeSeed.png';
+    } else if (plot.stage() === PlotStage.Sprout) {
+        return './images/farm/AllTreeSprout.png';
+    }
+    return `./images/${BerryType[plot.berry]}Tree${PlotStage[plot.stage()]}.png`;
+}
+
+const setPlotBerry = (berry) => {
+    if (selectedPlot() === undefined) {
+        return;
+    }
+    selectedPlot()._berry(berry);
+    setPlotStage(PlotStage.Berry);
+}
+
+const setPlotStage = (plotStage) => {
+    if (selectedPlot() === undefined) {
+        return;
+    }
+    if (plotStage == PlotStage.Seed) {
+        selectedPlot()._age(0);
+    } else {
+        const berryData = App.game.farming.berryData[selectedPlot()._berry()];
+        selectedPlot()._age(berryData.growthTime[plotStage]);
+    }
+}
+
+const getPossibleMutations = ko.pureComputed(() => {
+    const set = new Set();
+    App.game.farming.mutations.forEach((mutation) => {
+        const mutationPlots = mutation.getMutationPlots();
+        if (mutationPlots.length) {
+            set.add(BerryType[mutation.mutatedBerry]);
+        }
+    });
+    return Array.from(set);
+});
+
+const getExternalAuras = ko.pureComputed(() => {
+    const externalAuras = [];
+    App.game.farming.externalAuras.forEach((aura, idx) => {
+        if (!aura) {
+            return;
+        }
+        if (aura() !== 1 && idx !== AuraType.Repel) {
+            externalAuras.push(`${AuraType[idx]}: ×${aura().toFixed(2)}`);
+        } else if (aura() !== 0 && idx === AuraType.Repel) {
+            externalAuras.push(`${AuraType[idx]}: ${(aura() * 100).toFixed(2)}%`);
+        }
+    });
+
+    return externalAuras;
+});
+
+const getPossibleWanderers = ko.pureComputed(() => {
+    const set = new Set();
+    App.game.farming.plotList.forEach((plot) => {
+        if (plot.berry === BerryType.None) {
+            return;
+        }
+        plot.berryData.wander.forEach((w) => set.add(w));
+    });
+
+    // remove base wanderers
+    set.forEach((w) => {
+        if (Berry.baseWander.includes(w)) {
+            set.delete(w);
+        }
+    });
+
+    return Array.from(set);
+});
+
+const getHarvestAmount = () => {
+    if (!selectedPlot() || selectedPlot()._berry() == -1) {
+        return '-';
+    }
+    return selectedPlot().harvestAmount().toLocaleString();
+}
+
+const getFarmPointAmount = () => {
+    if (!selectedPlot() || selectedPlot()._berry() == -1) {
+        return '-';
+    }
+    return App.game.farming.berryData[selectedPlot().berry].farmValue.toLocaleString();
+}
+
+const getBerryColor = () => {
+    if (!selectedPlot() || selectedPlot()._berry() == -1) {
+        return '-';
+    }
+    return BerryColor[App.game.farming.berryData[selectedPlot().berry].color];
+}
+
+const getFlavorValue = (flavorType) => {
+    if (!selectedPlot() || selectedPlot()._berry() == -1) {
+        return '-';
+    }
+    return App.game.farming.berryData[selectedPlot().berry].flavors.find(f => f.type === flavorType).value;
+}
+
+const getStageTimes = ko.pureComputed(() => {
+    const stages = [
+        { stage: 'Sprout', timeLeft: '-' },
+        { stage: 'Taller', timeLeft: '-' },
+        { stage: 'Bloom', timeLeft: '-' },
+        { stage: 'Berry', timeLeft: '-' },
+        { stage: 'Wither', timeLeft: '-' },
+    ];
+
+    if (!selectedPlot() || selectedPlot()._berry() == -1) {
+        return stages;
+    }
+
+    stages.forEach((stage, idx) => {
+        const growthTime = App.game.farming.berryData[selectedPlot().berry].growthTime[idx];
+        const growthMultiplier = App.game.farming.getGrowthMultiplier() * selectedPlot().getGrowthMultiplier();
+        stages[idx].timeLeft = GameConstants.formatTimeFullLetters(growthTime / growthMultiplier);
+    });
+
+    return stages;
+});
+
+const getEmittingAura = ko.pureComputed(() => {
+    if (!selectedPlot() || selectedPlot()._berry() == -1 || selectedPlot().emittingAura.type() == null) {
+        return '-';
+    }
+    return `${AuraType[selectedPlot().emittingAura.type()]}: ${selectedPlot().berry === BerryType.Micle ? `+${(selectedPlot().emittingAura.value() * 100).toFixed(2)}%` : `×${selectedPlot().emittingAura.value().toFixed(2)}`}`;
+});
+
+const getReceivedAuras = ko.pureComputed(() => {
+    if (!selectedPlot() || selectedPlot()._berry() == -1) {
+        return '-';
+    }
+
+    const auras = selectedPlot().formattedAuras();
+    return auras || '-';
+});
+
+
+module.exports = {
+    selectedPlot,
+    getImage,
+    selectPlot,
+    setPlotBerry,
+    setPlotStage,
+    possibleMutations,
+    getPossibleMutations,
+    getExternalAuras,
+    getPossibleWanderers,
+    getHarvestAmount,
+    getFarmPointAmount,
+    getBerryColor,
+    getFlavorValue,
+    getStageTimes,
+    getEmittingAura,
+    getReceivedAuras,
+}
+},{}],116:[function(require,module,exports){
 
 const getBreedingAttackBonus = (vitaminsUsed, baseAttack) => {
     const attackBonusPercent = (GameConstants.BREEDING_ATTACK_BONUS + vitaminsUsed[GameConstants.VitaminType.Calcium]) / 100;
@@ -12963,7 +13138,7 @@ module.exports = {
     getBestVitamins,
 }
 
-},{}],116:[function(require,module,exports){
+},{}],117:[function(require,module,exports){
 const redirections = [
     ({type, name}) => {
         if (type === 'Pokemon') {
@@ -13015,7 +13190,7 @@ module.exports = {
     redirections
 };
 
-},{}],117:[function(require,module,exports){
+},{}],118:[function(require,module,exports){
 const { gotoPage } = require('./navigation');
 
 const searchOptions = [
