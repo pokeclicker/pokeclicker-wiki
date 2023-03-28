2adca185bc64fb998eda1330989fc0512916eacc
PixLSteam <PixLSteam@users.noreply.github.com>
2023-03-28 08:05:45 +0200

    Berry mutation rates (#35)
    * Berry mutation rates
    
    * fixup my broken update merge
    
    ---------
    
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>
    Co-authored-by: Danial <admin@redsparr0w.com>

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,7 @@ window.Wiki = {
   ...require('./markdown-renderer'),
   ...require('./discord'),
   pokemon: require('./pages/pokemon'),
+  farm: require('./pages/farm'),
   items: require('./pages/items'),
   dreamOrbs: require('./pages/dreamOrbs'),
   dungeons: require('./pages/dungeons'),
