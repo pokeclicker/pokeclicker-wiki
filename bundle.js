dfecabcfb352857cc9a79341ec3bb59e63fd8b69
Jessica Chen <jessica.scoltock@protonmail.com>
2023-04-16 03:09:25 +0100

    Deal Chains: Add to typeahead

--- bundle.js
+++ bundle.js
@@ -14128,6 +14128,12 @@ const searchOptions = [
     type: 'Daily Deals',
     page: '',
   },
+  // Deal Chains
+  {
+    display: 'Daily Deal Chains',
+    type: 'Daily Deal Chains',
+    page: ''
+  },
   // Weather
   {
     display: 'Weather',
