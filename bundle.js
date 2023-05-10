1e2a0839e7794e58a737d60f1b679a58aa3f3853
EatPant2nd <106291026+EatPant2nd@users.noreply.github.com>
2023-05-10 12:05:45 -0400

    Typeahead
    Typeahead, please. Also fixed a typo.

--- bundle.js
+++ bundle.js
@@ -14246,6 +14246,12 @@ const searchOptions = [
     type: 'Temporary Battles',
     page: '',
   },
+  // Wandering Pokémon
+  {
+    display: 'Wandering Pokémon',
+    type: 'Wandering Pokémon',
+    page: '',
+  },
 ];
 // Differentiate our different links with the same name
 searchOptions.forEach(a => {
