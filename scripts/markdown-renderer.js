a76e3f8b90e99d9586a85d5f57eaad1ff49c062c
Danial <admin@redsparr0w.com>
2023-08-17 16:20:11 +1200

    markdown-it-katex → markdown-it-mathjax3
    Final change, I hope 🤞

--- scripts/markdown-renderer.js
+++ scripts/markdown-renderer.js
@@ -13,7 +13,7 @@ const md = new markdownit({
     rightDelimiter: '}',
     allowedAttributes: ['id', 'class'],
   })
-  .use(require('@ryanxcharles/markdown-it-katex'))
+  .use(require('markdown-it-mathjax3'))
   .use(require('markdown-it-container'), 'text-center')
   .use(require('markdown-it-container'), 'text-end')
   .use(require('markdown-it-container'), 'table-auto')
