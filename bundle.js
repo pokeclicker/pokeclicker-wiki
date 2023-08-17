bc41bd3c61c222c946106e5512eedbd328ec1772
Sean Davis <wormania@gmail.com>
2023-08-17 22:31:32 +0100

    Add datatables order.neutral plugin and use it to allow resetting of sort order on datatables

--- bundle.js
+++ bundle.js
@@ -77192,6 +77192,23 @@ const applyDatatables = () => {
                   }
                 }
             });
+
+            // Setup a custom handler to reset sort order after descending instead of going back to ascending
+            // Accomplished by finding and intercepting the mutation of the sorting th's class from desc to asc
+            const callback = (mutationList) => {
+                for (const mutation of mutationList) {
+                    if (mutation.type === "attributes" &&
+                        mutation.attributeName == "class" &&
+                        mutation.oldValue.includes("sorting_desc") &&
+                        mutation.target.classList.contains("sorting_asc")
+                    ) {
+                        $(`#${element.id}`).DataTable().order.neutral().draw();
+                    }
+                }
+            };
+            document.querySelectorAll('th.sorting').forEach((el) => {
+                new MutationObserver(callback).observe(el, { attributes: true, attributeFilter: ['class'], attributeOldValue: true });
+            });
         } catch(e){}
     });
 }
