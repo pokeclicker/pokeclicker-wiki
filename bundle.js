c14b7842683c933ac0690ce6beaa9fb2eee1bfcb
Sean Davis <wormania@gmail.com>
2023-11-30 19:45:08 +0000

    Add alert when the page fails to load due to Knockout binding failure

--- bundle.js
+++ bundle.js
@@ -78188,6 +78188,19 @@ const alert = (message, type = 'primary', timeout = 5e3) => {
   }, timeout);
 }
 
+window.addEventListener("error", (event) => {
+  // Handle KO binding errors, usually occur when knockout is trying to use functions that
+  // don't exist in the cached javascript, but could also be a genuine error that slipped through
+  if (event?.error?.message?.startsWith("Unable to process binding")) {
+    alert(
+      `There was an error loading the page. Hard Refresh (Ctrl+F5 or Cmd+Shift+R) to clear the cache.
+      If this persists please post in #wiki-general on discord.`,
+      'danger',
+      3600e3
+    );
+  }
+});
+
 module.exports = {
   alert,
 };
