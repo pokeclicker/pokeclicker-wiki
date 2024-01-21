d17e0519054cdd2ee4d53558a098c7dbead43df6
Sean Davis <wormania@gmail.com>
2024-01-21 03:44:03 +0000

    Clean up shopMon code

--- bundle.js
+++ bundle.js
@@ -77861,10 +77861,8 @@ window.Wiki = {
   dreamOrbs: require('./pages/dreamOrbs'),
   farmSimulator: require('./pages/farmSimulator'),
   dungeons: require('./pages/dungeons'),
-  pokemonDollars: require('./pages/shopMon'),
+  shopMon: require('./pages/shopMon'),
   dungeonTokens: require('./pages/dungeonTokens'),
-  battlePoints: require('./pages/shopMon'),
-  diamonds: require('./pages/shopMon'),
   oakItems: require('./pages/oakItems'),
   getDealChains: require('./pages/dealChains').getDealChains,
   ...require('./navigation'),
@@ -78626,35 +78624,9 @@ const setWeather = (evt, weather) => {
     return;
 }
 
-const getShopMons = (currency) => {
-    var towns = Object.values(TownList).filter(t => t.region < GameConstants.Region.final);
-    var filteredTowns = [];
-    var filteredShops = [];
-    
-    for (var j = 0; j < towns.length; j++){
-        var test = towns[j].content.filter((c) => c instanceof Shop && c.items.length > 0);
-        if (test.length > 0) {
-            test.forEach(function(i) {
-                filteredTowns = [...filteredTowns, i];
-            });
-        }
-    }
-    
-    for (var k = 0; k < filteredTowns.length; k++){
-        var test1 = filteredTowns[k].items.filter((c) => c.currency == currency && c instanceof PokemonItem)
-        if (test1.length > 0) {
-            test1.forEach(function(s) {
-                filteredShops = [...filteredShops, [filteredTowns[k], s]];
-            });
-        }
-    }
-    return filteredShops;
-}
-
 module.exports = {
     highestRoute,
     setWeather,
-    getShopMons
 };
 },{}],523:[function(require,module,exports){
 const getTableClearCounts = (dungeon) => {
--- scripts/pages/dungeonTokens.js
+++ scripts/pages/dungeonTokens.js
@@ -85,33 +85,7 @@ const setWeather = (evt, weather) => {
     return;
 }
 
-const getShopMons = (currency) => {
-    var towns = Object.values(TownList).filter(t => t.region < GameConstants.Region.final);
-    var filteredTowns = [];
-    var filteredShops = [];
-    
-    for (var j = 0; j < towns.length; j++){
-        var test = towns[j].content.filter((c) => c instanceof Shop && c.items.length > 0);
-        if (test.length > 0) {
-            test.forEach(function(i) {
-                filteredTowns = [...filteredTowns, i];
-            });
-        }
-    }
-    
-    for (var k = 0; k < filteredTowns.length; k++){
-        var test1 = filteredTowns[k].items.filter((c) => c.currency == currency && c instanceof PokemonItem)
-        if (test1.length > 0) {
-            test1.forEach(function(s) {
-                filteredShops = [...filteredShops, [filteredTowns[k], s]];
-            });
-        }
-    }
-    return filteredShops;
-}
-
 module.exports = {
     highestRoute,
     setWeather,
-    getShopMons
 };
