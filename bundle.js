ea5f47e60ec49f761805dafae9f39765d6ac6013
CypherX <cypherx00@gmail.com>
2024-04-29 10:23:55 -0400

    Fix? for things with question marks in the name

--- bundle.js
+++ bundle.js
@@ -78101,7 +78101,7 @@ onhashchange = (event) => {
     pageElement.html(errorPage);
   });
 
-  const cleanFileName = (s) => s.replace(/[^\s\w\(\)'"!-é]/gi, "-");
+  const cleanFileName = (s) => s.replace(/[^\s\w\(\)'"!-é]/gi, "-").replace('?', '');
 
   const pageElementCustom = $('#wiki-page-custom-content');
   pageElementCustom.html('');
