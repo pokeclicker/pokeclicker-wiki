9379c86991c10324476cd74fadf8a59f0316696d
EatPant2nd <106291026+EatPant2nd@users.noreply.github.com>
2024-06-14 19:06:40 -0400

    NPCs
    Initial draft for NPCs page. Doesn't have a clickable table nor individual pages because, for now, it wouldn't be simple.

--- bundle.js
+++ bundle.js
@@ -79721,6 +79721,12 @@ const searchOptions = [
     type: 'Towns',
     page: t.name,
   })),
+  // NPCs
+  {
+    display: 'NPCs',
+    type: 'NPCs',
+    page: '',
+  },
   // Safari
   {
     display: 'Safari',
