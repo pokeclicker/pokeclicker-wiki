53a0073b8fca0bc5f34e7ab07cf3e3e9dc5d1bec
CypherX <672420+CypherX@users.noreply.github.com>
2025-06-25 17:51:17 -0400

    Game Updates page

--- bundle.js
+++ bundle.js
@@ -78304,7 +78304,7 @@ const md = new markdownit({
         const startCollapsed = m[0].startsWith('collapsed');
         // opening tag
         return `
-        <div class="accordion accordion-flush">
+        <div class="accordion">
           <div class="accordion-item">
             <h2 class="accordion-header">
               <button class="accordion-button ${startCollapsed ? 'collapsed' : ''}" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${randID}" aria-expanded="true">
