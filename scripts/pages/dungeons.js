7c74a5d7c359ff4b7bb08716a362a5062774adee
Sean Davis <wormania@gmail.com>
2026-09-13 22:26:36 +0100

    Show item drop amounts for rare hold items/dungeon drops

--- scripts/pages/dungeons.js
+++ scripts/pages/dungeons.js
@@ -220,6 +220,7 @@ const getDungeonLoot = (dungeon) => {
                 type: itemType,
                 image: itemGameData?.image ?? (pokemonData ? `assets/images/pokemon/${pokemonData.id}.png` : null),
                 weight: item.weight ?? 1,
+                amount: item.amount ?? 1,
                 requirement: item.requirement?.hint(),
                 ignoreDebuff: item.ignoreDebuff,
                 chances: []
@@ -237,6 +238,7 @@ const getDungeonLoot = (dungeon) => {
     }
     getDungeonLoot.cache.set(dungeon, lootTiers);
 
+    console.log(`getDungeonLoot(${dungeon.name})`, lootTiers);
     return lootTiers;
 };
 getDungeonLoot.cache = new WeakMap();
